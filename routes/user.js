c8ea992e019e8b12a8052facbd864d3c8b6bb4e6
TJ Barber <me@tjbarber.org>
2017-06-29 11:55:11 -0600

    get profile route added to api

---
+++ routes/user.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var request = require('request');
+var apiKeys = require('../lib/apiKeys');
+var router = express.Router();
+var redis  = require('../lib/redis');
+
+/* GET users listing. */
+router.get('/profile', function(req, res, next) {
+  var lowercasedGamertag = req.query['gamertag'].toLowerCase();
+  
+  redis.get(lowercasedGamertag, function(err, reply) {
+    if (reply) {
+      res.send(JSON.parse(reply));
+    } else {
+      var requestObject = {
+        url: 'https://xboxapi.com/v2/xuid/' + req.query['gamertag'],
+        headers: {
+          'Accept': 'application/json',
+          'X-AUTH': apiKeys.xboxLiveAPIKey
+        }
+      };
+
+      request(requestObject, function(error, response, body) {
+        if (error) {
+          var err = new Error('Unable to access server.');
+          err.status = 500;
+          next(err);
+        }
+    
+        var parsedBody = JSON.parse(body);
+
+        if (parsedBody['success'] === false) {
+          var err = new Error(parsedBody['error_message']);
+          err.status = parsedBody['error_code'];
+          next(err);
+        }
+
+        var xuid = parsedBody['xuid'];
+        var requestObject = {
+          url: 'https://xboxapi.com/v2/' + xuid + '/profile',
+          headers: {
+            'Accept': 'application/json',
+            'X-AUTH': apiKeys.xboxLiveAPIKey
+          }
+        }
+
+        request(requestObject, function(error, response, body) {
+          if (error) {
+            var err = new Error('Unable to access server.');
+            err.status = 500;
+            next(err);
+          }
+    
+          var parsedBody = JSON.parse(body);
+
+          if (parsedBody['success'] === false) {
+            var err = new Error(parsedBody['error_message']);
+            err.status = parsedBody['error_code'];
+            next(err);
+          }
+
+          redis.set(lowercasedGamertag, JSON.stringify(parsedBody), 'EX', 259200);
+          res.send(parsedBody);
+        });
+      });
+    }
+  });
+});
+
+module.exports = router;
