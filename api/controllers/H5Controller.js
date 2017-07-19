45c8f24b0181a9dc7041a15dc502831efbd14425
TJ Barber <me@tjbarber.org>
2017-07-18 20:43:55 -0600

    added quite a few routes, doing some data cleanup

--- api/controllers/H5Controller.js
+++ api/controllers/H5Controller.js
@@ -11,6 +11,11 @@ module.exports = {
       return res.send(data);
     });
   },
+  gameBaseVariants: function(req, res) {
+    Halo5Service.metadata.getGameBaseVariants({}, function(data) {
+      return res.send(data);
+    });
+  },
   matches: function(req, res) {
     Halo5Service.stats.getMatches({
       player: req.query['player'],
@@ -24,6 +29,23 @@ module.exports = {
   servicerecord: function(req, res) {
     Halo5Service.stats.getServiceRecord({player: req.query['player']}, function(data) {
       return res.send(data);
+    });
+  },
+  emblem: function(req, res) {
+    Halo5Service.profile.getEmblem({player: req.query['player'], size: '512'}, function(data) {
+      res.set('Content-Type', 'image/png');
+      res.send(data);
+    });
+  },
+  spartan: function(req, res) {
+    Halo5Service.profile.getSpartanImage({player: req.query['player']}, function(data) {
+      res.set('Content-Type', 'image/png');
+      res.send(data);
+    });
+  },
+  playlists: function(req, res) {
+    Halo5Service.metadata.getPlaylists({}, function(data) {
+      res.send(data);
     });
   }
 };
--- api/services/Halo5Service.js
+++ api/services/Halo5Service.js
@@ -22,7 +22,29 @@ module.exports = {
           body = "{}";
         }
 
-        return done(JSON.parse(body));
+        var parsedBody = JSON.parse(body);
+        var results = parsedBody['Results'];
+        var modifiedResults = [];
+
+        for (var i = 0; i < results.length; i++) {
+          var match = results[i];
+          var cleanedMatch = {};
+
+          cleanedMatch.id = match['Id'];
+          cleanedMatch.mapId = match['MapId'];
+          cleanedMatch.mapVariant = match['MapVariant'];
+          cleanedMatch.gameBaseVariantId = match['GameBaseVariantId'];
+          cleanedMatch.gameVariant = match['GameVariant'];
+          cleanedMatch.isTeamGame = match['IsTeamGame'];
+          cleanedMatch.playlistId = match['HopperId'];
+          cleanedMatch.queriedPlayer = match['Players'][0];
+          cleanedMatch.queriedPlayer.gamertag = cleanedMatch.queriedPlayer['Player']['Gamertag'];
+          cleanedMatch.seasonId = match['SeasonId'];
+
+          modifiedResults.push(cleanedMatch);
+        }
+
+        return done(modifiedResults);
       });
     },
     getServiceRecord: function(options, done) {
@@ -35,8 +57,18 @@ module.exports = {
         if (error) {
           return done(err)
         }
+        
+        var parsedBody = JSON.parse(body);
+        delete parsedBody['Results'][0]['Result']['ArenaStats']['ArenaPlaylistStats'];
+        delete parsedBody['Results'][0]['Result']['ArenaStats']['ArenaGameBaseVariantStats'];
+        delete parsedBody['Results'][0]['Result']['ArenaStats']['Impulses'];
+        delete parsedBody['Results'][0]['Result']['ArenaStats']['WeaponStats'];
+        delete parsedBody['Results'][0]['Result']['ArenaStats']['MedalAwards'];
+        delete parsedBody['Results'][0]['Result']['ArenaStats']['DestroyedEnemyVehicles'];
 
-        return done(JSON.parse(body));
+        var prunedData = parsedBody['Results'][0];
+
+        return done(prunedData);
       });
     }
   },
@@ -54,6 +86,68 @@ module.exports = {
         }
 
         return done(JSON.parse(body));
+      });
+    },
+    getGameBaseVariants: function(options, done) {
+      var requestObject = {
+        url: 'https://www.haloapi.com/metadata/h5/metadata/game-base-variants',
+        headers: standardHeaders,
+        gzip: true
+      }
+
+      sails.request(requestObject, function(error, response, body) {
+        if (error) {
+          return done(error);
+        }
+
+        return done(JSON.parse(body));
+      });
+    },
+    getPlaylists: function(options, done) {
+      var requestObject = {
+        url: 'https://www.haloapi.com/metadata/h5/metadata/playlists',
+        headers: standardHeaders,
+        gzip: true
+      }
+
+      sails.request(requestObject, function(error, response, body) {
+        if (error) {
+          return done(error);
+        }
+
+        return done(JSON.parse(body));
+      });
+    }
+  },
+  profile: {
+    getEmblem: function(option, done) {
+      var requestObject = {
+        url: "https://www.haloapi.com/profile/h5/profiles/" + option.player + "/emblem?size=" + option.size,
+        headers: standardHeaders,
+        encoding: null
+      }
+
+      sails.request(requestObject, function(error, response, body) {
+        if (error) {
+          return done(error);
+        }
+        
+        return done(body);
+      });
+    },
+    getSpartanImage: function(option, done) {
+      var requestObject = {
+        url: "https://www.haloapi.com/profile/h5/profiles/" + option.player + "/spartan?size=512",
+        headers: standardHeaders,
+        encoding: null
+      }
+
+      sails.request(requestObject, function(error, response, body) {
+        if (error) {
+          return done(error);
+        }
+        
+        return done(body);
       });
     }
   }
